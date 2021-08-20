51cdb3ca0aac061038bb9f9528bef68b73e4a84c
Peter <pierrekahumuza@outlook.com>
2021-08-20 09:19:44 -0700

    initial commit

---
+++ config/config.jp.php
@@ -0,0 +1,12 @@
+<?php
+$numero = "(+243) 991-163-777";
+$prooject_name = "zuri";
+$number_of_children = 4;
+
+$today = date('Y-m-d');
+
+$con = mysqli_connect("localhost", "root", "", "zuridb") or die("Couln't connect to the database");
+
+$errors = [];
+
+?>
