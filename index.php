62ea050b05f6d897909e39d90b0d3dc6cd2931f5
Peter <pierrekahumuza@outlook.com>
2021-08-20 08:42:41 -0700

    master commit

--- index.php
+++ index.php
@@ -330,8 +330,6 @@
         </div>
     </div>
 </section>
-
-
 <section class="blog-area section-gap" id="blog">
     <div class="container">
         <div class="row d-flex justify-content-center">
